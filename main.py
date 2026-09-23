19cfe430077780aa1f4090a23a3af4bacb053d26
noelnatale <natalen29@setoncatholic.org>
2026-09-23 20:14:39 +0000

    made a calculator

---
+++ main.py
@@ -0,0 +1,23 @@
+# this function adds two numbers
+def add(x,y):
+    print(x + y)
+
+# this function subtracts two numbers
+def sub(x,y):
+    print(x - y)
+
+# this function multiplies two numbers
+def mul(x,y):
+    print(x * y)
+
+# this function divides two numbers
+def div(x,y):
+    print(x / y)
+
+##############################################################################################
+x = int(input("Enter your first number:"))
+y = int(input("Enter your second number:"))
+add(x,y)
+sub(x,y)
+mul(x,y)
+div(x,y)
